e121b72c0e1254ce7948bf1e871340c3f2ffb8bf
lalocesar2003 <ceed1000@gmail.com>
2024-06-28 12:32:53 -0500

    toca revisar todo el video y busacar el repositorio original

---
+++ data/index.ts
@@ -0,0 +1,8 @@
+import { title } from "process";
+
+export const gridItems = [
+    {title: 'Title 1', description: 'Description 1',id:1},
+    {title: 'Title 1', description: 'Description 1',id:1},
+    {title: 'Title 1', description: 'Description 1',id:1},
+    {title: 'Title 1', description: 'Description 1',id:1},
+]
